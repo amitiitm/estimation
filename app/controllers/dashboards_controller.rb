91dc0f1854e00597f544d24a4641c594220fa581
Amit Pandey <amitiitm>
2019-09-06 13:19:37 +0530

    Modify dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,9 @@ class DashboardsController < ApplicationController
 	before_action :authenticate_user
 
   def index
+    @users = User.all
+    @clients = Client.all
+    @estimations = Estimation.all
+    @total_hours = Estimation.all.map {|e| e.estimation_templates.sum(&:final_hours) }.sum 
   end
 end
